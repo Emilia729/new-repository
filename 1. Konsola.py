a36dedd926c41486e1519a6f9941ef481f53bac6
Emilia729 <emilia.jaros01@gmail.com>
2022-04-11 17:38:15 +0200

    new repository

---
+++ 1. Konsola.py
@@ -0,0 +1,7 @@
+wzrost=1.72
+masa=73
+wiek=29
+stala=-161
+tryb=1.4
+print("BMI wynosi: ", masa/wzrost**2)
+print("Dzienne zapotrzebowanie kaloryczne wynosi: ", 10*masa+6.25*wzrost*100-5*wiek+stala)
